4c8cb09206f50007efc73f692f04e914296a51ca
b947023cc <81670168+b947023cc@users.noreply.github.com>
2021-03-31 22:47:30 +0800

    Update waifu-tips.js

--- waifu-tips.js
+++ waifu-tips.js
@@ -97,7 +97,7 @@ function loadWidget(config) {
 			showMessage("你都复制了些什么呀，转载要记得加上出处哦！", 6000, 9);
 		});
 		window.addEventListener("visibilitychange", () => {
-			if (!document.hidden) showMessage("哇，你终于回来了～", 6000, 9);
+			if (!document.hidden) showMessage("哇，你终于回来了修改過～", 6000, 9);
 		});
 	})();
 
